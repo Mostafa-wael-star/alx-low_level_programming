c3dd3cadd6ff0e6e377730d24dc7ce8bed75a05b
Mostafa <mostafa.almallah@gmail.com>
2023-09-07 17:14:01 -0700

    first c prg.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the programe
+ *
+ * Description : A C program that print something using puts finction
+ *
+ * Return : Always 0 otherwise it's not successful
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
