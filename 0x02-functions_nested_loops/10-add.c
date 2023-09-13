dcf7bcdd173e939bad5bbcee34c2bf85e6e51b22
Mostafa <mostafa.almallah@gmail.com>
2023-09-13 03:31:30 +0200

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - function
+ *
+ * Desciption: retuen the add of two numbers
+ *
+ * Return: sum of 2 num
+ *
+ * @i1 : number 1
+ * @i2 : number 2
+*/
+
+int add(int i1, int i2)
+{
+return (i1 + i2);
+}
