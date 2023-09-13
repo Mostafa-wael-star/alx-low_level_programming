e654a24ba86027542c05c0c9e903d3cb9a73642a
Mostafa <mostafa.almallah@gmail.com>
2023-09-13 03:14:58 +0200

    lastdigit 2

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,12 +13,14 @@
 
 int print_last_digit(int n)
 {
-if (n < 0)
-{_putchar((-n % 10) + '0');
-return ((-n % 10));
+int last_digit;
+
+last_digit = n % 10;
+if (last_digit < 0)
+{
+last_digit = last_digit * -1;
 }
-else
-{_putchar((n % 10) + '0');
-return ((n % 10));
-}
+_putchar(last_digit + '0');
+return (last_digit);
+
 }
