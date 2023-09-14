05d43ef28d24ec67d30cfa3d9357fdbb0057593b
Mostafa <mostafa.almallah@gmail.com>
2023-09-14 17:44:11 +0300

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *mul - function
+ *
+ *Description : Detirmines if the letter is upper or lower
+ *
+ *@a : first number
+ *@b : second number
+ *Return: if Upper case charachter ruturn (1) if lower case return (0)
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
