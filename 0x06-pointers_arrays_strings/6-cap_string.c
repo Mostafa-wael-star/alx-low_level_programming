5383beda8ceb3c3440852f01aaa054e34b1a7aee
Mostafa <mostafa.almallah@gmail.com>
2023-09-21 23:57:19 +0300

    cap first letter

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * cap_string - Capitlize first letter
+ *
+ * @str: Capitlize first letter
+ *
+ * Return: pointer
+ */
+
+char *cap_string(char *str)
+{
+int i = 1;
+
+if (str[0] <= 'z' && str[0] >= 'a')
+str[i] = str[i] - 32;
+
+for (; str[i] != '\0'; i++)
+{
+switch (str[i - 1])
+{
+case ' ':
+case '\n':
+case '\t':
+case '{':
+case '}':
+case ',':
+case ';':
+case '.':
+case '?':
+case '"':
+case '(':
+case ')':
+if (str[i] <= 'z' && str[i] >= 'a')
+str[i] = str[i] - 32;
+break;
+}
+}
+return (str);
+}
