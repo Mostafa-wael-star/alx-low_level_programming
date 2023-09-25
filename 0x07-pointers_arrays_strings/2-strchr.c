4d6ae5946f7ba8d02cd431f1e895a129d7d295bd
Mostafa <mostafa.almallah@gmail.com>
2023-09-25 21:41:03 +0300

    strchr update

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,14 +12,13 @@
 
 char *_strchr(char *s, char c)
 {
-while (*s != '\0')
+int i = 0;
+
+for (; s[i] >= '\0'; i++)
 {
-if (*s == c)
-{
-return (s);
+if (s[i] == c)
+return (&s[i]);
 }
-else
-s++;
-}
-return ('\0');
+return (0);
+
 }
