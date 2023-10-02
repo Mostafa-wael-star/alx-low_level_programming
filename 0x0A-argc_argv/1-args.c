7f501658ac5e1161283af74617729c6400b7ba93
Mostafa <mostafa.almallah@gmail.com>
2023-10-02 18:43:26 +0300

    args number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * @argc: the size of the arguments
+ * @argv: the arguments itself
+ * Return: always 0 success
+*/
+
+
+int main(int argc, char *argv[])
+{
+printf("%i\n", argc - 1);
+}
