db2b7c75d7407e39766288fb80a8cc71c70f408d
Mostafa <mostafa.almallah@gmail.com>
2023-10-02 18:39:27 +0300

    what's my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * @argc: the size of the arguments
+ * @argv: the arguments itself
+ * Return: always 0 success
+*/
+
+
+int main(int argc, char *argv[])
+{
+printf("%s\n" , argv[argc = 0]);
+return (0);
+}
